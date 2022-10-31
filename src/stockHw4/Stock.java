483c2f26ea15f6a31f4381ba3e944680994eb752
Bharat Chawla <35283839+bharatchawla53@users.noreply.github.com>
2022-10-31 01:51:41 -0400

    hw4 javaDoc

--- src/stockHw4/Stock.java
+++ src/stockHw4/Stock.java
@@ -1,17 +1,22 @@
 package stockHw4;
 
+/**
+ * Demonstrates a command-line-based Stock application.
+ */
 public class Stock {
 
   public static void main(String[] args) {
     System.out.println("Stock app started");
 
+    // create the model
     StockModel model = new StockModelImpl();
+    // create the view
     StockView view = new StockViewImpl();
+    // create the controller
     StockController controller = new StockControllerImpl(model, view);
-
+    // hand over the control of the app to the controller
     controller.start();
 
     System.out.println("Stock app ended");
   }
-
 }
